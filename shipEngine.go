313028bd6f2ca9e1538e0d8b30fee1a113927e6a
Come Wasik <wasik.come@gmail.com>
2021-09-16 16:40:21 +0200

    Add begin code to generate a ship

--- shipEngine.go
+++ shipEngine.go
@@ -25,3 +25,36 @@ func hasCollission(boats []Ship, x int8, y int8) bool {
 	}
 	return false
 }
+
+// Min x 0
+// Min y 0
+// Max x 9
+// Max y 9
+
+func createGame() {
+	// ships := make([]Ship, 0, 5)
+
+	// ship = generateShip(ships, 2)
+	// ship = generateShip(ships, 3)
+}
+
+func generateShip(ships []Ship, shipSize int8) Ship {
+	var ship Ship
+	positions := make([]ShipPosition, 0, 5)
+	for i := 0; i < int(shipSize); i++ {
+		var position ShipPosition
+		searchPosition := true
+		for searchPosition {
+			// Generate coord
+
+			// Verify collission
+			if !hasCollission(ships, position.x, position.y) {
+				searchPosition = false
+			}
+		}
+		positions = append(positions, position)
+	}
+
+	ship.positions = positions
+	return ship
+}
