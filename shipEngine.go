96155f65b5c0789c5a592a3d42df0e7e08fe0b8d
mlima95 <milan.lima@hotmail.fr>
2021-09-16 17:15:18 +0200

    Init shipEngine

--- shipEngine.go
+++ shipEngine.go
@@ -1,5 +1,7 @@
 package main
 
+import "math/rand"
+
 // Position of a ship
 type ShipPosition struct {
 	x int8
@@ -9,7 +11,80 @@ type ShipPosition struct {
 // A ship
 type Ship struct {
 	// Positions for this ship
-	Positions []ShipPosition
+	positions []ShipPosition
 	// Positions where the ship is touched
-	TouchedAt []ShipPosition
+	touchedAt []ShipPosition
+}
+
+// Checks if a shot has a hit
+func hasCollission(boats []Ship, x int8, y int8) bool {
+	for i := 0; i < len(boats); i++ {
+		for j := 0; j < len(boats[i].positions); j++ {
+			if x == boats[i].positions[j].x && y == boats[i].positions[j].y {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+// Min x 0
+// Min y 0
+// Max x 9
+// Max y 9
+
+func createGame() {
+	// ships := make([]Ship, 0, 5)
+
+	// ship = generateShip(ships, 2)
+	// ship = generateShip(ships, 3)
+}
+
+func generateShip(ships []Ship, shipSize int8) Ship {
+	var ship Ship
+	positions := make([]ShipPosition, 0, 5)
+	const HORIZONTAL = 0
+	const VERTICAL = 0
+	orientation := int(rand.Intn(1))
+	var position ShipPosition
+	if orientation == HORIZONTAL {
+		x := int8(rand.Intn(9 - int(shipSize)))
+		y := int8(rand.Intn(9))
+		position.x = x
+		position.y = y
+		positions = append(positions, position)
+		for i := 0; i < int(shipSize); i++ {
+			position.x = x + int8(i)
+			position.y = y
+			positions = append(positions, position)
+		}
+	} else if orientation == VERTICAL {
+		x := int8(rand.Intn(9))
+		y := int8(rand.Intn(9 - int(shipSize)))
+		position.x = x
+		position.y = y
+		positions = append(positions, position)
+		for i := 0; i < int(shipSize); i++ {
+			position.x = x
+			position.y = y + int8(i)
+			positions = append(positions, position)
+		}
+	}
+
+	for i := 0; i < int(shipSize); i++ {
+		var position ShipPosition
+		searchPosition := true
+		for searchPosition {
+			// Generate coord
+
+			// Verify collission
+			if !hasCollission(ships, position.x, position.y) {
+				searchPosition = false
+			}
+		}
+		positions = append(positions, position)
+	}
+
+	ship.positions = positions
+	return ship
 }
