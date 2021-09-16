f28334471618e145906c7fb995777850cf30c665
JACQUENET Christophe <jjacquenet@myges.fr>
2021-09-16 15:29:14 +0200

    add code structure

---
+++ shipEngine.go
@@ -0,0 +1,15 @@
+package main
+
+// Position of a ship
+type ShipPosition struct {
+	x int8
+	y int8
+}
+
+// A ship
+type Ship struct {
+	// Positions for this ship
+	Positions []ShipPosition
+	// Positions where the ship is touched
+	TouchedAt []ShipPosition
+}
