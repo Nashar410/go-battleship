037922e8a70259e8f990ddc3ac852c34d7837da4
JACQUENET Christophe <jjacquenet@myges.fr>
2021-09-16 15:23:28 +0200

    add ship and position

--- main.go
+++ main.go
@@ -5,3 +5,18 @@ import "fmt"
 func main() {
 	fmt.Println()
 }
+
+
+// Position of a ship
+type ShipPosition struct {
+	x int8
+	y int8
+}
+
+// A ship
+type Ship struct {
+	// Positions for this ship
+	Positions []ShipPosition
+	// Positions where the ship is touched
+	TouchedAt []ShipPosition
+}
